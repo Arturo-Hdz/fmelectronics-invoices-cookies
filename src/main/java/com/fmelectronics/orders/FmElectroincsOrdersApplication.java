148121ebb4d4080a42e4553f40a162c5cdb3b70a
Arturo-Hdz <arbaherpe07@hotmail.com>
2024-10-17 16:05:43 -0400

    first commit

---
+++ src/main/java/com/fmelectronics/orders/FmElectroincsOrdersApplication.java
@@ -0,0 +1,13 @@
+package com.fmelectronics.orders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmElectroincsOrdersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmElectroincsOrdersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fmelectronics/orders/FmElectroincsOrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmelectronics.orders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmElectroincsOrdersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
